c587f5729693dd1ad6a8ee134f6bcab84c332101
Andre Piton <andre.elias.vieira@gmail.com>
2020-09-16 16:59:16 -0300

    adicionando main da loteria e do dicionario - exercicio 1

--- src/main/kotlin/collections/exercicios/exercicio1/Main.kt
+++ src/main/kotlin/collections/exercicios/exercicio1/Main.kt
@@ -1,5 +1,16 @@
 package collections.exercicios.exercicio1
 
 fun main() {
+    var loteria = Loteria_Sonhos(mapOf(0 to "ovo",
+            1 to "ovo",
+            2 to "ovo",
+            3 to "ovo",
+            4 to "ovo",
+            5 to "ovo"))
 
+    loteria.primeiros_cinco()
+
+    var dicionario = DicionarioApelido()
+
+    dicionario.dicionario()
 }
